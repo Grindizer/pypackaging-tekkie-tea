40058d5a7309a55affd96c531d618b39557bff86
nassim <nassim.babaci@cloudreach.co.uk>
2017-09-21 23:45:17 +0200

    sub

--- pycloudchecker/cloudchecker/checker/runner.py
+++ pycloudchecker/cloudchecker/checker/runner.py
@@ -14,11 +14,6 @@ def iter_checks():
 
 
 def run_checker(session):
-    """
-    Perform all elb checks on all elbs for a given region.
-    :param session: boto3 session.
-    :return: generator that yield the results as a tuple (<name of the check>, <elb name>, <check result>)
-    """
     client = session.client('elb')
     for elb in list_load_balancer(client):
         for check_name, check in list_elb_checks():
